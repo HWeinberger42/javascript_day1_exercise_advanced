30bc63d569499c1c3a7332a61cc267445033d80c
Hermann Weinberger <miol@protonmail.com>
2024-09-17 14:28:17 +0200

    finish 3.1

---
+++ script.js
@@ -0,0 +1,28 @@
+// exercise 3.1
+
+let a = 7; 
+let b = 14; 
+let c = "21"; 
+let d = '36'; 
+let e = 42;
+
+console.log(`${a}, ${b}, ${c}, ${d}, ${e}`);
+
+let sum = a + b + c + d + e;
+
+console.log(sum);
+
+let f = '1';
+let g = 15;
+let h = 8;
+let i = "1";
+
+console.log(`${f}, ${g}, ${h}, ${i}`);
+
+let product = f * g * h * i;
+
+console.log(product);
+
+let fraction = sum / product;
+
+document.write(fraction);
